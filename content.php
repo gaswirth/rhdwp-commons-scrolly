5b7ef92cfc6469922d6ae33bc2084b5cbdf52835
Nick Gaswirth <nick@roundhouse-designs.com>
2015-05-06 10:48:27 -0400

    Tweaks

--- content.php
+++ content.php
@@ -9,7 +9,7 @@
 
 	<li <?php post_class( 'news-item' ); ?>>
 		<?php if ( has_post_thumbnail() ) : ?>
-			<a href="<?php echo do_shortcode('[ct id="_ct_text_5537ffb9ac86e" property="value"]'); ?>" target="_blank">
+			<a href="<?php echo do_shortcode('[ct id="###CUSTOMPRESS ID###" property="value"]'); ?>" target="_blank">
 				<?php the_post_thumbnail( 'news' ); ?>
 			</a>
 			<?php the_content(); ?>
