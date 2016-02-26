8f5a836569ebbd129e2cabf19cacbb2061a66a7c
Nick Gaswirth <nick@roundhouse-designs.com>
2016-02-26 14:54:37 -0500

    Removed matt murphy info

--- footer.php
+++ footer.php
@@ -16,7 +16,7 @@
         <?php get_sidebar( 'footer' ); ?>
         <div class="site-info">
 			<p>
-				<?php echo '&copy;' . date( 'Y' ); ?> <?php echo bloginfo( 'name' ); ?> <?php echo ( rhd_is_mobile() ) ? '<br>' : '|&nbsp;'; ?>Site photography by <a href="//murphymade.com" target="_blank">Matt Murphy</a> <?php echo ( rhd_is_mobile() ) ? '<br>' : '|&nbsp;'; ?>Site by <a href="//roundhouse-designs.com" target="_blank">Roundhouse<img id="rhd-logo-footer" src="//assets.roundhouse-designs.com/images/rhd-black-house.png" alt="Roundhouse Designs">Designs</a>
+				<?php echo '&copy;' . date( 'Y' ); ?> <?php echo bloginfo( 'name' ); ?> <?php echo ( rhd_is_mobile() ) ? '<br>' : '|&nbsp;'; ?>Site by <a href="//roundhouse-designs.com" target="_blank">Roundhouse<img id="rhd-logo-footer" src="//assets.roundhouse-designs.com/images/rhd-black-house.png" alt="Roundhouse Designs">Designs</a>
             </p>
         </div><!-- .site-info -->
     </footer><!-- #colophon -->
